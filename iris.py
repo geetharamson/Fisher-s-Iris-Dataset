3f2995e858cc2ba69958c365f97b4bcb3f63a701
geetharamson <47304216+geetharamson@users.noreply.github.com>
2019-04-14 13:57:30 +0100

    Create iris.py

---
+++ iris.py
@@ -0,0 +1,10 @@
+    
+#GEETHA KARTHIKESAN - 2019
+#Fisher Iris data set
+#https://archive.ics.uci.edu/ml/machine-learning-databases/iris/
+#https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.describe.html
+
+#Import the libraries needed to explore the data set
+ import numpy as np
+ import pands as pd 
+ import matplotlib.pyplot as plt
